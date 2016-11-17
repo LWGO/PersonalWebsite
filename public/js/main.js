7525297b2bb0b69beea24b24bc7f3ac19d4d996c
Lars Osterberg <losterberg@hubspot.com>
2016-11-16 22:17:37 -0500

    first attempt at FB ads reach

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  getFacebookPhoto();
+  getAdsTargeting();
 });
+
+function getAdsTargeting() {
+  var url = 'GET https://graph.facebook.com/v2.8/search?type=adinterest&q=soccer&access_token=' + apiKey;
+  $.ajax(url, {
+    success: function (data) {
+      $('.photo').text(data.name);
+    }
+  });
+}
